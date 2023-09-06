af8bb4b30a7ffb373a5b0c589328eba1ddb2b643
luisdavid0 <ldsoto69@misena.edu.co>
2023-09-06 11:33:12 -0500

    primero

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function texto(){
+    document.getElementById('s3').innerHTML= '<h1> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque expedita aut aspernatur at illum soluta dolor praesentium quidem repellat in! Accusantium officiis maxime est nesciunt ab hic similique ratione enim?</h1>'
+}
+function f3 (){
+    swal({
+        title: "Bien hecho",
+        icon: "success",
+        button: "Salir"
+    });
+}
